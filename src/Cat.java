0ade73cfcc747afc9fddaacd4237d6ee089cf527
kosukyung <kolyn092@gmail.com>
2026-03-05 23:54:38 +0900

    fix: @Override 키워드 추가

--- src/Cat.java
+++ src/Cat.java
@@ -6,10 +6,12 @@ public class Cat extends Animal {
         super(name, age);
     }
 
+    @Override
     public String GetType() {
         return "고양이";
     }
 
+    @Override
     public void Sound() {
         System.out.println(GetName() + ": 야옹~");
     }
