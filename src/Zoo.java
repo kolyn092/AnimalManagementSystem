687d7d5df9eb02dd142d838a6edc89510764f30d
kosukyung <kolyn092@gmail.com>
2026-03-05 17:36:43 +0900

    fix: 코드 중복 제거

--- src/Zoo.java
+++ src/Zoo.java
@@ -48,87 +48,84 @@ public class Zoo {
         }
     }
 
-    // 놀아주기
-    public static void PlayWithAnimal(Scanner scanner) {
+    // 동물 선택 함수
+    public static Animal SelectAnimal(Scanner scanner, String str) {
         // 동물 목록
         ShowAnimalList();
 
         // 입력
-        System.out.print("놀아 줄 동물을 선택하세요: ");
+        System.out.print(str + "동물을 선택하세요: ");
         int selectIndex = scanner.nextInt();
 
         // 인덱스 벗어나는 경우 처리
         if (selectIndex - 1 >= animalList.size() || selectIndex - 1 < 0) {
             System.out.println("잘못된 번호입니다.");
-            return;
+            return null;
         }
 
         // 선택한 동물 정보 가져오기
-        var targetAnimal = animalList.get(selectIndex - 1);
+        return animalList.get(selectIndex - 1);
+    }
+
+    // 놀아주기
+    public static void PlayWithAnimal(Scanner scanner) {
+        // 선택한 동물 정보 가져오기
+        var targetAnimal = SelectAnimal(scanner, "놀아 줄 ");
+
+        // 잘못된 번호를 선택해서 가져온 정보가 없을 경우
+        if (targetAnimal == null) {
+            return;
+        }
+
+        // 놀아주기
         var nowHappiness = targetAnimal.Play();
 
+        // 결과 출력
         System.out.println(targetAnimal.GetName() + "와 놀기 - 행복도가 증가합니다. (" + nowHappiness + ")");
     }
 
     // 먹이주기
     public static void FeedAnimal(Scanner scanner) {
-        // 동물 목록
-        ShowAnimalList();
+        // 선택한 동물 정보 가져오기
+        var targetAnimal = SelectAnimal(scanner, "먹이 줄 ");
 
-        // 입력
-        System.out.print("먹이 줄 동물을 선택하세요: ");
-        int selectIndex = scanner.nextInt();
-
-        // 인덱스 벗어나는 경우 처리
-        if (selectIndex - 1 >= animalList.size() || selectIndex - 1 < 0) {
-            System.out.println("잘못된 번호입니다.");
+        // 잘못된 번호를 선택해서 가져온 정보가 없을 경우
+        if (targetAnimal == null) {
             return;
         }
 
-        // 선택한 동물 정보 가져오기
-        var targetAnimal = animalList.get(selectIndex - 1);
+        // 먹이주기
         var nowHungry = targetAnimal.Eat();
 
+        // 결과 출력
         System.out.println(targetAnimal.GetName() + "가 먹습니다 - 배고픔이 감소합니다. (" + nowHungry + ")");
     }
 
     // 울음소리 듣기
     public static void ListenSound(Scanner scanner) {
-        // 동물 목록
-        ShowAnimalList();
+        // 선택한 동물 정보 가져오기
+        var targetAnimal = SelectAnimal(scanner, "울음소리를 들을 ");
 
-        // 입력
-        System.out.print("울음소리를 들을 동물을 선택하세요: ");
-        int selectIndex = scanner.nextInt();
-
-        // 인덱스 벗어나는 경우 처리
-        if (selectIndex - 1 >= animalList.size() || selectIndex - 1 < 0) {
-            System.out.println("잘못된 번호입니다.");
+        // 잘못된 번호를 선택해서 가져온 정보가 없을 경우
+        if (targetAnimal == null) {
             return;
         }
 
-        // 선택한 동물 정보 가져오기
-        var targetAnimal = animalList.get(selectIndex - 1);
+        // 소리 듣기
         targetAnimal.Sound();
     }
 
     // 동물 상태 확인
     public static void CheckAnimalState(Scanner scanner) {
-        // 동물 목록
-        ShowAnimalList();
+        // 선택한 동물 정보 가져오기
+        var targetAnimal = SelectAnimal(scanner, "상태를 확인할 ");
 
-        // 입력
-        System.out.print("상태를 확인할 동물을 선택하세요: ");
-        int selectIndex = scanner.nextInt();
-
-        // 인덱스 벗어나는 경우 처리
-        if (selectIndex - 1 >= animalList.size() || selectIndex - 1 < 0) {
-            System.out.println("잘못된 번호입니다.");
+        // 잘못된 번호를 선택해서 가져온 정보가 없을 경우
+        if (targetAnimal == null) {
             return;
         }
 
-        // 선택한 동물 정보 가져오기
-        var targetAnimal = animalList.get(selectIndex - 1);
+        // 상태 출력
         System.out.println("배고픔: " + targetAnimal.GetHungry() + " 행복도: " + targetAnimal.GetHappiness());
     }
 }
